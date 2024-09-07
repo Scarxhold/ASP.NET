94e377e446d9916e96a78e972102454f38aab106
Scarxhold <118264138+Scarxhold@users.noreply.github.com>
2024-09-07 16:20:06 +0300

    Add files via upload

---
+++ Controllers/CompanyController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompanyController : ControllerBase
+    {
+        private readonly Company _company;
+
+        public CompanyController()
+        {
+            _company = new Company
+            {
+                Id = 1,
+                Name = "Example Company",
+                Address = "123 Main St, City, Country",
+                Industry = "Technology"
+            };
+        }
+
+        // GET: api/company
+        [HttpGet]
+        public IActionResult GetCompany()
+        {
+            return Ok(_company);
+        }
+    }
+}
---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RandomNumberController : ControllerBase
+    {
+        private readonly Random _random;
+
+        public RandomNumberController()
+        {
+            _random = new Random();
+        }
+
+        [HttpGet]
+        public IActionResult GetRandomNumber()
+        {
+            int randomNumber = _random.Next(0, 101);
+            return Ok(randomNumber);
+        }
+    }
+}
